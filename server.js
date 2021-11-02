e636437763c25fc2829e501798db623bfc0af43d
Ali Madooei <alimadooei@gmail.com>
2021-11-01 21:56:09 -0400

    Step 6: HTTP Module

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
+const http = require("http");
 
-fs.readFile("./index.html", "utf8", (err, files) => {
-  console.log(err ? err : files);
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(`<h1>Hello HTTP Module!</h1>`);
+  response.end();
 });
+
+server.listen(7000);
+console.log("Listening! (port 7000)");
